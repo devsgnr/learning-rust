58530ad9bd40e7e473af5475ae01f51fd97d2a93
Emmanuel Watila <ei.watila@gmail.com>
2023-03-13 02:14:40 +0100

    Worked on String & Maps as Collection and changed file structure

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ pub mod coin;
 pub mod message;
 pub mod shape;
 
+use std::collections::HashMap;
+
 use coin::coin::{check_coin_option, check_if_coin, value_in_cents, Coin};
 use message::message::Message;
 use shape::shape::Rectangle;
@@ -67,6 +69,46 @@ fn main() {
     ];
 
     println!("{:?}", list);
+
+    /*
+     String as a collection
+    */
+    let mut s = String::from("Emmanuel");
+    s.push_str(" Watila");
+    println!("{}", s);
+
+    let s1 = String::from("Tic");
+    let s2 = String::from("Tac");
+    let s3 = String::from("Toe");
+    let s_concat = format!("{s1}-{s2}-{s3}");
+
+    println!("{}", s_concat);
+
+    let s_literal = "Emmanuel";
+    let s_bytes = &s_literal.as_bytes();
+    for (i, &item) in s_bytes.iter().enumerate() {
+        println!("{}: {}", i, item);
+    }
+    for (i, character) in s_literal.chars().enumerate() {
+        println!("{}: {}", i, character)
+    }
+    println!("{:?}", s_bytes);
+
+    /*
+     Collections -> HashMap
+    */
+    let mut map: HashMap<&str, u32> = HashMap::new();
+
+    map.insert("Blue", 10);
+    map.insert("Yellow", 20);
+
+    map.entry("Green").or_insert(25);
+
+    println!("{:?}", map);
+
+    for (key, value) in map {
+        println!("{}: {}", key, value);
+    }
 }
 
 /*
